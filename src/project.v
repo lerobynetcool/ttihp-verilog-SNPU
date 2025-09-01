e633cecd153699622be256ca0dea9355281d75e5
Laurent Roro <le.robynet.cool@gmail.com>
2025-09-01 21:18:44 +0200

    minimal test

--- src/project.v
+++ src/project.v
@@ -41,25 +41,26 @@ module tt_um_SNPU (
   input  wire       clk,      // clock
   input  wire       rst_n     // reset_n - low to reset
 );
-  assign uio_oe  = 1; // use io pins as outputs
+  assign uio_oe  = 0;
+  // assign uio_oe  = 1; // use io pins as outputs
 
-  funky_rnd rnd00 (.G(ui_in[0]),.R(uo_out[0]));
-  funky_rnd rnd01 (.G(ui_in[0]),.R(uo_out[1]));
-  funky_rnd rnd02 (.G(ui_in[0]),.R(uo_out[2]));
-  funky_rnd rnd03 (.G(ui_in[0]),.R(uo_out[3]));
-  funky_rnd rnd04 (.G(ui_in[0]),.R(uo_out[4]));
-  funky_rnd rnd05 (.G(ui_in[0]),.R(uo_out[5]));
-  funky_rnd rnd06 (.G(ui_in[0]),.R(uo_out[6]));
-  funky_rnd rnd07 (.G(ui_in[0]),.R(uo_out[7]));
+  // funky_rnd rnd00 (.G(ui_in[0]),.R(uo_out[0]));
+  // funky_rnd rnd01 (.G(ui_in[0]),.R(uo_out[1]));
+  // funky_rnd rnd02 (.G(ui_in[0]),.R(uo_out[2]));
+  // funky_rnd rnd03 (.G(ui_in[0]),.R(uo_out[3]));
+  // funky_rnd rnd04 (.G(ui_in[0]),.R(uo_out[4]));
+  // funky_rnd rnd05 (.G(ui_in[0]),.R(uo_out[5]));
+  // funky_rnd rnd06 (.G(ui_in[0]),.R(uo_out[6]));
+  // funky_rnd rnd07 (.G(ui_in[0]),.R(uo_out[7]));
 
-  funky_rnd rnd10 (.G(ui_in[0]),.R(uio_out[0]));
-  funky_rnd rnd11 (.G(ui_in[0]),.R(uio_out[1]));
-  funky_rnd rnd12 (.G(ui_in[0]),.R(uio_out[2]));
-  funky_rnd rnd13 (.G(ui_in[0]),.R(uio_out[3]));
-  funky_rnd rnd14 (.G(ui_in[0]),.R(uio_out[4]));
-  funky_rnd rnd15 (.G(ui_in[0]),.R(uio_out[5]));
-  funky_rnd rnd16 (.G(ui_in[0]),.R(uio_out[6]));
-  funky_rnd rnd17 (.G(ui_in[0]),.R(uio_out[7]));
+  // funky_rnd rnd10 (.G(ui_in[0]),.R(uio_out[0]));
+  // funky_rnd rnd11 (.G(ui_in[0]),.R(uio_out[1]));
+  // funky_rnd rnd12 (.G(ui_in[0]),.R(uio_out[2]));
+  // funky_rnd rnd13 (.G(ui_in[0]),.R(uio_out[3]));
+  // funky_rnd rnd14 (.G(ui_in[0]),.R(uio_out[4]));
+  // funky_rnd rnd15 (.G(ui_in[0]),.R(uio_out[5]));
+  // funky_rnd rnd16 (.G(ui_in[0]),.R(uio_out[6]));
+  // funky_rnd rnd17 (.G(ui_in[0]),.R(uio_out[7]));
 
   wire _unused = &{uio_in,clk,rst_n,ena, 1'b0};
 endmodule
