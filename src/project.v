6f361365a54de9fd4ba8930d0edaa2a7c48576f7
Laurent Roro <le.robynet.cool@gmail.com>
2025-09-01 19:51:53 +0200

    trying reg stuff

--- src/project.v
+++ src/project.v
@@ -67,7 +67,7 @@ endmodule
 // tt_um_SNPU stands for "Tiny Tapeout - Secret Nigon Processing Unit"
 module tt_um_SNPU (
     input  wire [7:0] ui_in,    // Dedicated inputs
-    output reg  [7:0] uo_out,   // Dedicated outputs
+    output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
     output reg  [7:0] uio_out,  // IOs: Output path
     output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
@@ -89,8 +89,11 @@ module tt_um_SNPU (
   reg [10:0] players_party;
   reg [10:0] players_nigonitude;
 
+  // reg [7:0] reg_output;
+
   wire [2:0] op_code;
   assign op_code = ui_in[7:5];  // Use the top 3 bits of A to determine the operation
+
   // always @(*) begin
   always @(posedge clk or negedge rst_n) begin
     if (!rst_n) begin
@@ -99,8 +102,8 @@ module tt_um_SNPU (
       N_discard <= 0 ; // cf [board game state]
       // players_party <= 0; // TODO
       // players_nigonitude<=0; // TODO
+      // reg_output <=0; // TODO : I think we don't care
       uio_out <=0; // TODO : I think we don't care
-      uo_out  <=0; // TODO : I think we don't care
     end
     // else case (op_code)
     //   // 3'b000: uo_out = A + B;           // Addition
@@ -127,7 +130,8 @@ module tt_um_SNPU (
 
   // All output pins must be assigned. If not used, assign to 0.
   assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
+  // assign uio_out = reg_output;
+  // assign uio_out = 0;
   assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
