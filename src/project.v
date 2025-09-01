44299c91aa0537998d919d395c4719b06cc54a03
Laurent Roro <le.robynet.cool@gmail.com>
2025-09-01 19:28:12 +0200

    use reg instead of wire

--- src/project.v
+++ src/project.v
@@ -67,9 +67,9 @@ endmodule
 // tt_um_SNPU stands for "Tiny Tapeout - Secret Nigon Processing Unit"
 module tt_um_SNPU (
     input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
+    output reg  [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
+    output reg  [7:0] uio_out,  // IOs: Output path
     output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
     input  wire       ena,      // always 1 when the design is powered, so you can ignore it
     input  wire       clk,      // clock
@@ -89,10 +89,6 @@ module tt_um_SNPU (
   reg [10:0] players_party;
   reg [10:0] players_nigonitude;
 
-  // stored state for outputs
-  reg [7:0] reg_uio;
-  reg [7:0] reg_uo;
-
   wire [2:0] op_code;
   assign op_code = ui_in[7:5];  // Use the top 3 bits of A to determine the operation
   // always @(*) begin
@@ -103,8 +99,8 @@ module tt_um_SNPU (
       N_discard <= 0 ; // cf [board game state]
       // players_party <= 0; // TODO
       // players_nigonitude<=0; // TODO
-      reg_uio <=0; // TODO : I think we don't care
-      reg_uo  <=0; // TODO : I think we don't care
+      uio_out <=0; // TODO : I think we don't care
+      uo_out  <=0; // TODO : I think we don't care
     end
     // else case (op_code)
     //   // 3'b000: uo_out = A + B;           // Addition
