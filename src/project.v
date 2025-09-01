d052bdf669815b235319859def80158515fa3a3a
Laurent Roro <le.robynet.cool@gmail.com>
2025-09-01 21:04:02 +0200

    random module

--- src/project.v
+++ src/project.v
@@ -5,8 +5,6 @@
 
 `default_nettype none
 
-/* === RANDOM STUFF === */
-
 module nand_latch (
   input wire S,
   input wire R,
@@ -33,47 +31,15 @@ module funky_rnd(
   );
 endmodule
 
-// module pseudo_rnd(
-//   input wire clk,
-//   input wire rst,
-//   output reg [7:0] R
-// )
-//   reg [7:0] lfsr;
-//   wire feedback;
-
-//   // Feedback polynomial: x^8 + x^6 + x^5 + x^4 + 1
-//   assign feedback = lfsr[7] ^ lfsr[5] ^ lfsr[4] ^ lfsr[3];
-
-//   always @(posedge clk or posedge rst) begin
-//     if (rst) begin
-//       lfsr <= 8'h1;  // Non-zero seed
-//     end else begin
-//       lfsr <= {lfsr[6:0], feedback};
-//     end
-//   end
-
-//   // Output current LFSR value as pseudo-random value
-//   always @(*) begin
-//     rand_val = lfsr;
-//   end
-// endmodule
-
-/*  */
-
-// ===================
-// === MAIN MODULE ===
-// ===================
-
-// tt_um_SNPU stands for "Tiny Tapeout - Secret Nigon Processing Unit"
 module tt_um_SNPU (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+  input  wire [7:0] ui_in,    // Dedicated inputs
+  output wire [7:0] uo_out,   // Dedicated outputs
+  input  wire [7:0] uio_in,   // IOs: Input path
+  output wire [7:0] uio_out,  // IOs: Output path
+  output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+  input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+  input  wire       clk,      // clock
+  input  wire       rst_n     // reset_n - low to reset
 );
   assign uio_oe  = 1; // use io pins as outputs
 
@@ -96,60 +62,4 @@ module tt_um_SNPU (
   funky_rnd rnd17 (.G(ui_in[0]),.R(uio_out[7]));
 
   wire _unused = &{clk,rst_n,ena, 1'b0};
-
-  // assign uio_oe  = 0; // use io pins as inputs, we don't need that much outputs anyway
-
-  // // board game state
-  // reg [16:0] policies; // 17 policy cards with 6x 0 and 11x 1.
-  // reg [5:0] N_stack;   // initialized to 17 : number of policies in the main stack
-  // reg [5:0] N_discard; // initialized to 0  : number of cards that are in the hand of a player
-  // // policies  : [ 0 1 1 0 1 1 1║0 1 0 1 1┊0 1 0 1 1 ]
-  // // N_stack   : ═══════════════╝         ┊           
-  // // N_discard :                └─────────┘           
-  // //              [STACK]        [DISCARD] [ BOARD ]
-
-  // // role distribution for game initialization
-  // reg [10:0] players_party;
-  // reg [10:0] players_nigonitude;
-
-  // reg [7:0] reg_outputA; // internal state for output
-  // assign uo_out = reg_outputA;
-
-  // // list of operations :
-  // localparam OP_RSET = 3'b000; // - OP_reset()                    : policies=00000000000111111, N_stack=17, N_hand=0, N_discard = 0
-  // localparam OP_PLRS = 3'b001; // - OP_player_reset(player_count) : initialize player array to 000...01...1 and another array with 000...01, they will be shuffled together
-  // localparam OP_PLGT = 3'b010; // - OP_player_get(index)          : shows if player is type 11 (liberal) or 10 (Nigon) or 00 (nigonist)
-  // localparam OP_SHFF = 3'b011; // - OP_shuffle()                  : shuffles some bits in [0..(N_stack+N_discard)] ; need to run many time to get an actual shuffle
-  // localparam OP_HDSP = 3'b100; // - OP_hand_display(index)        : shows the card at N_stack-1-index
-  // localparam OP_HDSC = 3'b101; // - OP_hand_discard(index)        : shift all registers from N_stack-1-index etc...
-  // localparam OP_HPLY = 3'b110; // - OP_hand_play(index)           : shift all registers from N_stack-1-index etc...
-  // localparam OP_BDSP = 3'b111; // - OP_board_display()            : outputs two 4 bit integers (how many ZEROS and how many ONES inside the BOARD section)
-
-  // wire [2:0] op_code;
-  // assign op_code = ui_in[7:5];  // Use the top 3 bits of A to determine the operation
-
-  // always @(posedge clk or negedge rst_n) begin
-  //   // reg_outputA <= op_code;
-  //   if (!rst_n || op_code == OP_RSET) begin
-  //     policies  <= 17'b00000000000111111; // cf [board game state]
-  //     N_stack   <= 17; // cf [board game state]
-  //     N_discard <= 0 ; // cf [board game state]
-  //     // players_party <= 0; // TODO
-  //     // players_nigonitude<=0; // TODO
-  //     // reg_outputA <=0;
-  //   end else case (op_code)
-  //     OP_PLRS: reg_outputA <= OP_PLRS; // TODO
-  //     OP_PLGT: reg_outputA <= OP_PLGT; // TODO
-  //     OP_SHFF: reg_outputA <= OP_SHFF; // TODO
-  //     OP_HDSP: reg_outputA <= OP_HDSP; // TODO
-  //     OP_HDSC: reg_outputA <= OP_HDSC; // TODO
-  //     OP_HPLY: reg_outputA <= OP_HPLY; // TODO
-  //     OP_BDSP: reg_outputA <= OP_BDSP; // TODO
-  //     default: reg_outputA = 8'b00000000;
-  //   endcase
-  // end
-
-  // All output pins must be assigned. If not used, assign to 0.
-  // List all unused inputs to prevent warnings
-  // wire _unused = &{ena, 1'b0};
 endmodule
--- test/test.py
+++ test/test.py
@@ -5,7 +5,6 @@ import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
 
-
 @cocotb.test()
 async def test_project(dut):
     dut._log.info("Start")
@@ -13,15 +12,6 @@ async def test_project(dut):
     # Set the clock period to 10 us (100 KHz)
     clock = Clock(dut.clk, 10, units="us")
     cocotb.start_soon(clock.start())
-
-    OP_RSET = 0<<5
-    OP_PLRS = 1<<5
-    OP_PLGT = 2<<5
-    OP_SHFF = 3<<5
-    OP_HDSP = 4<<5
-    OP_HDSC = 5<<5
-    OP_HPLY = 6<<5
-    OP_BDSP = 7<<5
 
     # Reset
     dut._log.info("Reset")
@@ -33,48 +23,29 @@ async def test_project(dut):
 
     dut.rst_n.value = 1
 
+    # generate random values, please go crazy :D
     dut.uio_in.value = 0
     for i in range(30):
         await ClockCycles(dut.clk, 1)
         print(dut.ui_in.value,dut.uo_out.value,dut.uio_out.value)
 
+    # now the thing should freeze to the same output always :D
     dut.uio_in.value = 255
     for i in range(30):
         await ClockCycles(dut.clk, 1)
         print(dut.ui_in.value,dut.uo_out.value,dut.uio_out.value)
 
 
-
     # dut.rst_n.value = 1
+
+    # dut._log.info("Test project behavior")
+
+    # # Set the input values you want to test
+    # dut.ui_in.value = 20
+    # dut.uio_in.value = 30
+
+    # # Wait for one clock cycle to see the output values
     # await ClockCycles(dut.clk, 1)
-    # async def hello(OP_CODE):
-    #     dut.ui_in.value = OP_CODE
-    #     await ClockCycles(dut.clk, 1)
-    #     print(dut.uo_out.value,dut.ui_in.value,OP_CODE)
-    #     await ClockCycles(dut.clk, 1)
-    #     print(dut.uo_out.value,dut.ui_in.value,OP_CODE)
-
-
-    # await hello(OP_RSET)
-    # await hello(OP_PLRS)
-    # await hello(OP_PLGT)
-    # await hello(OP_SHFF)
-    # await hello(OP_HDSP)
-    # await hello(OP_HDSC)
-    # await hello(OP_HPLY)
-    # await hello(OP_BDSP)
-
-
-    dut.rst_n.value = 1
-
-    dut._log.info("Test project behavior")
-
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
-
-    # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
 
     # The following assersion is just an example of how to check the output values.
     # Change it to match the actual expected output of your module:
